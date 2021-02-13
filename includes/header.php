86735ec9502398bd34d42cc9a5d9fe2d48c936aa
Santigp258 <santigp258@gmail.com>
2021-02-12 19:38:23 -0500

    template

---
+++ includes/header.php
@@ -0,0 +1,52 @@
+<?php include_once('./enviroments.php') ?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Easy Food</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="icon" href="icon.svg">
+
+  <link rel="stylesheet" href="<?php echo BASE_URL ?>/css/normalize.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL ?>/css/main.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+</head>
+
+<body>
+  <!-- Menu de navegación -->
+  <nav class="navbar">
+    <div class="container">
+      <a href="#"><img src="img/logoEasyFood.svg" alt='' class="logo"></a>
+      <div class="navbarContent" id="navbarContent">
+        <ul class="navbar-nav">
+          <li class="nav-item-nicio">
+            <a class="nav-link" href="#">Inicio</a>
+          </li>
+          <li class="nav-item-menu">
+            <a class="nav-link" href="menu.php">Menú</a>
+          </li>
+          <li class="nav-item-nosotros">
+            <a class="nav-link" href="#NosotrosPart">
+              Nosotros
+            </a>
+          </li>
+          <li class="nav-item-ubicanos">
+            <a class="nav-link" href="#UbicanosPart">Ubícanos</a>
+          </li>
+          <li class="nav-item-carrito">
+            <a class="nav-link" href="#"><img src="img/shopping-cart.svg" alt="" class="carritoDeCompras"></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php include_once('./includes/header.php');  ?> 
+  <!-- Nosotros -->
+  <div class="NosotrosPart">
+    <h1 id="NosotrosPart">Nosotros</h1>
+    <div class="circulos">
+      <img src="img/circle1.svg" alt="" class="circleOfNosotros">
+      <img src="img/circle1.svg" alt="" class="circleOfNosotros">
+      <img src="img/circle1.svg" alt="" class="circleOfNosotros">
+    </div>
+    <div class="textNosotros">
+      <h1></h1><p>Somos Easy Food, nos encargamos de llevarte a tu domicilio 
+      las mejores pizzas tamaño familiar, además de ser super deliciosas
+      son las mejores de nuestra ciudad.</p>
+      <p>Nosotros nos encargaremos de llevarte hasta la puerta de tu casa la pizza que deseas a un precio imperdible. Somos Easy Food, pizzas deliciosas y rápidas.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae illo quidem quaerat sit praesentium quos commodi quas, facilis doloribus eius nihil quia modi?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem harum nulla dolorem. Repellendus molestias necessitatibus deserunt id hic! Recusandae!</p>
+    </div>
+    <div class="pizza">
+      <img src="img/pizzaEntera.svg" alt="" class="pizzaEntera">
+      <h2>Easy Food</h2>
+      <h3>Pizzas deliciosas y rápidas</h3>
+    </div>
+  </div>
+  <!-- Ubícanos -->
+  <div class="UbicanosPart">
+    <h1 id="UbicanosPart">Ubícanos</h1>
+    <div class="circulos">
+      <img src="img/circle1.svg" alt="" class="circleOfNosotros">
+      <img src="img/circle1.svg" alt="" class="circleOfNosotros">
+      <img src="img/circle1.svg" alt="" class="circleOfNosotros">
+    </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.6679393972568!2d-73.97952668459395!3d40.74733167932805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259068214096b%3A0x5b182b7357ff1c20!2sLibretto&#39;s%20Pizza!5e0!3m2!1ses-419!2spe!4v1613102977862!5m2!1ses-419!2spe" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" class="mapa"></iframe>
+  </div>
+
+  <?php include_once('./includes/footer.php');  ?> 
+
